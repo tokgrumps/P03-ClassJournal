51e127c5b3d8e9b521581d6ff152a81dd41a5b15
Juhan Jufri <15017395@myrp.edu.sh>
2017-05-05 12:41:15 +0800

    infoPage updates 2

--- app/src/main/java/com/myapplicationdev/android/p03_classjournal/infoPage.java
+++ app/src/main/java/com/myapplicationdev/android/p03_classjournal/infoPage.java
@@ -2,12 +2,20 @@ package com.myapplicationdev.android.p03_classjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class infoPage extends AppCompatActivity {
+    Button btnAdd, btnInfo, btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
+
+        btnAdd = (Button) findViewById(R.id.buttonAdd);
+        btnInfo = (Button) findViewById(R.id.buttonInfo);
+        btnEmail = (Button) findViewById(R.id.buttonEmail);
+
+
     }
 }
