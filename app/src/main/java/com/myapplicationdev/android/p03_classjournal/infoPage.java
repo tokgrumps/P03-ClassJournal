1dbcc462fcee5ae695c86f46687244a5a2207170
Juhan Jufri <15017395@myrp.edu.sh>
2017-05-05 15:04:27 +0800

    more updates

--- app/src/main/java/com/myapplicationdev/android/p03_classjournal/infoPage.java
+++ app/src/main/java/com/myapplicationdev/android/p03_classjournal/infoPage.java
@@ -58,7 +58,29 @@ public class infoPage extends AppCompatActivity {
             }
         });
 
+        btnEmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT,
+                        "Test Email from C347");
+
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
+            }});
     }
+
+
 
 
 
