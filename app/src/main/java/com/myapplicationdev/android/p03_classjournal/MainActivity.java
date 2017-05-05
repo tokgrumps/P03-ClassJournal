469f7f4b73f6d6f97396dfd3a89ad44568aaab93
15017573 <15017573@myrp.edu.sg>
2017-05-05 12:42:37 +0800

    Added ListView

--- app/src/main/java/com/myapplicationdev/android/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p03_classjournal/MainActivity.java
@@ -1,13 +1,41 @@
 package com.myapplicationdev.android.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lvModule;
+
+
+    ArrayList<String> module = new ArrayList<String>();
+    ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvModule = (ListView) this.findViewById(R.id.ListViewModule);
+
+        module = new ArrayList<String>();
+        module.add(new String("C347"));
+
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, module);
+        lvModule.setAdapter(aa);
+
+        lvModule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, infoPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
